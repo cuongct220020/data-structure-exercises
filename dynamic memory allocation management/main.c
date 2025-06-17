b651aef94e912306abf98408381408628a7358a6
Cuongit1-hust <cuongct0902@gmail.com>
2025-06-17 20:06:07 +0700

    add memory management exercise

--- dynamic memory allocation management/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "memblock.h"
-
-int main() {
-
-    return 0;
-}
---
+++ memory management/build/CMakeFiles/memory_management.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memory_management.dir/src/main.c.o"
+  "CMakeFiles/memory_management.dir/src/main.c.o.d"
+  "CMakeFiles/memory_management.dir/src/mem_alloc.c.o"
+  "CMakeFiles/memory_management.dir/src/mem_alloc.c.o.d"
+  "memory_management"
+  "memory_management.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/memory_management.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
